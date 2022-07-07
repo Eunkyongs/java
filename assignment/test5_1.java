c429b13ad76547f431f2a113f4275bc04ad7ea25
Eunkyongs <106173646+Eunkyongs@users.noreply.github.com>
2022-07-08 00:17:47 +0900

    Rename test5_1.java to assignment/test5_1.java

--- assignment/test5_1.java
+++ assignment/test5_1.java
@@ -5,12 +5,12 @@ import java.util.Set;
 
 /*[TEST1]
 
-��ǻ�Ͱ� 1~9���� ���ڸ� �� 5�� �ݺ� �մϴ�.
-5�� �ݺ��ϴ� ���� �迭�� �ش� ���� ���� �Ǿ�� �մϴ�. ��, �ߺ��� ���� ���� �迭�� �Է� ���� �ʵ��� �մϴ�.
-�� ��Ʈ
-�迭�� ArrayList�� ����ϸ�, �迭�� �ߺ��� ���δ� ������ return �޼ҵ带 Ȱ���Ͽ� üũ �ǵ��� �մϴ�.
+컴퓨터가 1~9까지 숫자를 총 5번 반복 합니다.
+5번 반복하는 동안 배열에 해당 값이 저장 되어야 합니다. 단, 중복된 값은 절대 배열에 입력 되지 않도록 합니다.
+※ 힌트
+배열은 ArrayList를 사용하며, 배열에 중복값 여부는 별도의 return 메소드를 활용하여 체크 되도록 합니다.
 
-���� ���� [2,3,7,9,4]*/
+정답 예시 [2,3,7,9,4]*/
 public class test5_1 {
 	public static void main(String[] args) {
 		repeatnum rn= new repeatnum();
